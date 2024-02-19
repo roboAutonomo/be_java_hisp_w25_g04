1c809bc1824b55d9bffc8f5ebaf79368a7e749f2
luchoRodriguez26 <157501189+luchoRodriguez26@users.noreply.github.com>
2024-02-19 15:09:44 -0300

    Revert "Feature/us 0002"

--- src/main/java/com/breakingbytes/be_java_hisp_w25_g04/repository/IUserRepository.java
+++ src/main/java/com/breakingbytes/be_java_hisp_w25_g04/repository/IUserRepository.java
@@ -1,11 +1,4 @@
 package com.breakingbytes.be_java_hisp_w25_g04.repository;
 
-import com.breakingbytes.be_java_hisp_w25_g04.dto.request.UserDTO;
-import com.breakingbytes.be_java_hisp_w25_g04.entity.Seller;
-
-import java.util.List;
-
 public interface IUserRepository {
-
-    Seller getSellerById(int idSeller);
 }
