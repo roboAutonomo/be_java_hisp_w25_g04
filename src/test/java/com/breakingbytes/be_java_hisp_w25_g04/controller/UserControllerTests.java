f5d772dd4fc765957b1a1bb50b88d41e4a644c2f
Gustavo-meli <gustavojavier.ferreyra@mercadolibre.com>
2024-02-27 18:09:50 -0300

    UserService tests

--- src/test/java/com/breakingbytes/be_java_hisp_w25_g04/controller/UserControllerTests.java
+++ src/test/java/com/breakingbytes/be_java_hisp_w25_g04/controller/UserControllerTests.java
@@ -1,4 +1,37 @@
 package com.breakingbytes.be_java_hisp_w25_g04.controller;
 
+import com.breakingbytes.be_java_hisp_w25_g04.entity.Post;
+import com.breakingbytes.be_java_hisp_w25_g04.entity.Seller;
+import com.breakingbytes.be_java_hisp_w25_g04.entity.User;
+import com.breakingbytes.be_java_hisp_w25_g04.repository.SellerRepositoryImpl;
+import com.breakingbytes.be_java_hisp_w25_g04.service.UserServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTests {
+
+    @Mock
+    UserServiceImpl userService;
+
+    @InjectMocks
+    UserController userController;
+
+    @Test
+    @DisplayName("T-0001")
+    public void followUserTestOk() {
+        // arrange
+        Integer userId = 1;
+        Integer userIdToFollow = 2;
+
+    }
 }
