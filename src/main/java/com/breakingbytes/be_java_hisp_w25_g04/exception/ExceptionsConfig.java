1989ca42e236a76d137034af05b1364eec276d8b
lrodriguezlo <luciano.rodriguezloza@mercadolibre.com>
2024-03-01 11:43:51 -0300

    correcciones excepciones de validaciones

--- src/main/java/com/breakingbytes/be_java_hisp_w25_g04/exception/ExceptionsConfig.java
+++ src/main/java/com/breakingbytes/be_java_hisp_w25_g04/exception/ExceptionsConfig.java
@@ -1,6 +1,7 @@
 package com.breakingbytes.be_java_hisp_w25_g04.exception;
 
 import com.breakingbytes.be_java_hisp_w25_g04.dto.response.ExceptionDTO;
+import com.breakingbytes.be_java_hisp_w25_g04.dto.response.ExceptionValidatorsDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionsConfig {
@@ -26,7 +32,12 @@ public class ExceptionsConfig {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e){
-        ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
+
+        // e.getFieldError().getDefaultMessage() --> Con esto se ve el mensaje de error de la validacion,
+        // pero.. ¿como vemos si hay varias validaciones?
+        List<ExceptionValidatorsDTO> validatorsDTO = new ArrayList<>();
+        e.getFieldErrors().forEach(fieldError -> validatorsDTO.add(new ExceptionValidatorsDTO(fieldError.getField(), fieldError.getDefaultMessage())));
+        ExceptionDTO exceptionDto = new ExceptionDTO("Hay campos invalidos" ,validatorsDTO);
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(HttpMessageNotReadableException.class)
