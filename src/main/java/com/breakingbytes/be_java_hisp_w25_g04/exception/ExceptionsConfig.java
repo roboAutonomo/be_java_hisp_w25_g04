041f4071841a222729996661d713f0ada85236ee
GonzaloVega-MELI <gonzaloariel.vega@mercadolibre.com>
2024-03-01 10:39:37 -0300

    Se agregan handler para excepciones de las validaciones

--- src/main/java/com/breakingbytes/be_java_hisp_w25_g04/exception/ExceptionsConfig.java
+++ src/main/java/com/breakingbytes/be_java_hisp_w25_g04/exception/ExceptionsConfig.java
@@ -3,6 +3,8 @@ package com.breakingbytes.be_java_hisp_w25_g04.exception;
 import com.breakingbytes.be_java_hisp_w25_g04.dto.response.ExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +19,17 @@ public class ExceptionsConfig {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionDTO> badRequest(BadRequestException e) {
+        ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e){
+        ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> httpMessageNotReadableException(HttpMessageNotReadableException e){
         ExceptionDTO exceptionDto = new ExceptionDTO(e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
